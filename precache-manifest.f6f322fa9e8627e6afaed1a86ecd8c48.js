2ea0035efa662a041342820d5420c6f59768dbec
Martin Milani <martin.milani.mdq@gmail.com>
2020-07-18 21:20:27 -0300

    Updates

---
+++ precache-manifest.f6f322fa9e8627e6afaed1a86ecd8c48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b086481164a89e67c18921088f00172",
+    "url": "/martinmilani/drum-machine/index.html"
+  },
+  {
+    "revision": "b45d6eaf4cd38e8b10c4",
+    "url": "/martinmilani/drum-machine/static/js/2.d62e3503.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/martinmilani/drum-machine/static/js/2.d62e3503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6481b0ff0188eabbb9d3",
+    "url": "/martinmilani/drum-machine/static/js/main.58eb7bf0.chunk.js"
+  },
+  {
+    "revision": "f1708a5a08a723afbba4",
+    "url": "/martinmilani/drum-machine/static/js/runtime-main.3ccd8b4c.js"
+  }
+]);
